af5295241cc7abee5d0bf255406a258170513945
Auxeedee <kolyakrivokon@gmail.com>
2025-05-22 21:20:06 +0300

    lab

---
+++ lab13/lab13.js
@@ -0,0 +1,8 @@
+// Два початкові масиви
+const array1 = [1, 2, 3];
+const array2 = [4, 5, 6];
+
+// Об'єднання масивів за допомогою spread
+const mergedArray = [...array1, ...array2];
+
+console.log(mergedArray);
